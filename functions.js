7a3b5a886e0a6dbb88f006f14271ed7128a88bae
na_da <20108524+NadineDalecki@users.noreply.github.com>
2022-06-07 12:00:24 +0300

    affen +spreadsheet

--- functions.js
+++ functions.js
@@ -112,16 +112,16 @@ module.exports = {
                 newEmbed.setFooter(embed[0].Footer_Text, embed[0].Footer_Avatar_URL);
             }
             if (embed[0].Field_1_Title && embed[0].Field_1_Text) {
-                newEmbed.addField(embed[0].Field_1_Title, embed[0].Field_1_Text);
+                newEmbed.addField({name: embed[0].Field_1_Title, value: embed[0].Field_1_Text});
             }
             if (embed[0].Field_2_Title && embed[0].Field_2_Text) {
-                newEmbed.addField(embed[0].Field_2_Title, embed[0].Field_2_Text);
+                newEmbed.addField({name: embed[0].Field_2_Title, value: embed[0].Field_2_Text});
             }
             if (embed[0].Field_3_Title && embed[0].Field_3_Text) {
-                newEmbed.addField(embed[0].Field_3_Title, embed[0].Field_3_Text);
+                newEmbed.addField({name: embed[0].Field_3_Title, value: embed[0].Field_3_Text});
             }
             if (embed[0].Field_4_Title && embed[0].Field_4_Text) {
-                newEmbed.addField(embed[0].Field_4_Title, embed[0].Field_4_Text);
+                newEmbed.addField({name: embed[0].Field_4_Title, value: embed[0].Field_4_Text});
             }
             if (embed[0].Field_5_Title && embed[0].Field_5_Text) {
                 newEmbed.addField({text: embed[0].Field_5_Title, iconURL: embed[0].Field_5_Text});
