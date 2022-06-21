3cb5712702ac2d7af11f65cc856e5d8c79d74067
na_da <20108524+NadineDalecki@users.noreply.github.com>
2022-06-21 16:10:10 +0300

    new date

--- commands/KVN test.js
+++ commands/KVN test.js
@@ -10,20 +10,19 @@ module.exports = {
             const cells = await sheet.loadCells('B2:D5')
            
           try {
-               /* let userRow = rows.filter(row => row["Discord Username"] == message.author.tag);
+               let userRow = rows.filter(row => row["Discord Username"] == message.author.tag);
                 //console.log(userRow[0])
-                userRow[0]["Test 31.03.2022"] = "1"
+                userRow[0]["Test 26.07.2022"] = "1"
                 await userRow[0].save();
                 message.react("✅")
-                message.author.send("I added you to our list. We will send out Oculus invitations about 30 minutes before the test. Don't forget to check our <#783287669613527041> if you run into any issues or ask for help in <#724969092506976286>! looking forward to see you in the Stadium")*/
-              message.author.send("Hm. Sorry I can't sign you up right now. We have to wait until another test is announced! 👀")
+                message.author.send("I added you to our list. We will send out Oculus invitations about 30 minutes before the test. Don't forget to check our <#783287669613527041> if you run into any issues or ask for help in <#724969092506976286>! looking forward to see you in the Stadium")
+              //message.author.send("Hm. Sorry I can't sign you up right now. We have to wait until another test is announced! 👀")
             }
             catch (e) {
-              /*  message.author.send("Looks like I have trouble finding your Confidentiality Agreement. 🤔 Did you maybe change your Discord tag (name#1234)? You can update it in your form submission and try again afterwards!")*/
+                message.author.send("Looks like I have trouble finding your Confidentiality Agreement. 🤔 Did you maybe change your Discord tag (name#1234)? You can update it in your form submission and try again afterwards!")
             }
         }
         }
     }
 };
 
-//message.author.send("Hm. Sorry I can't sign you up right now. We have to wait until another test is announced! 👀")
