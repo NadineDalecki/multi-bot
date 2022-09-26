425e6bd6658a8c462fc0eaf3ee4216ef241de266
na_da <20108524+NadineDalecki@users.noreply.github.com>
2022-09-26 16:44:50 +0300

    test update

--- commands/KVN test.js
+++ commands/KVN test.js
@@ -1,39 +1,31 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 module.exports = {
-    name: "test",
-    async execute(client, message, functions, args, set) {
-             if (client.user.username === "KVN") {
+  name: "test",
+  async execute(client, message, functions, args, set) {
+      {
+           if (client.user.username === "KVN") {
+          const data = await functions.SpreadsheetGET(client);
 
-              const doc = new GoogleSpreadsheet("1y_M-lWfdLIH9F2mFlwh0wSZXCd6SWxaKg7-5t555gNc");
-              await doc.useServiceAccountAuth({
-                  client_email:
-                      process.env[`CLIENT_EMAIL_${client.user.username.toUpperCase()}`],
-                  private_key: process.env[
-                      `PRIVATE_KEY_${client.user.username.toUpperCase()}`
-                  ].replace(/\\n/g, "\n"),
-              });
-              await doc.loadInfo();
+          const sheet = data.doc.sheetsByTitle["Form responses 1"];
+          const rows = await sheet.getRows();
+          const cells = await sheet.loadCells('B2:D5')
+         
+        try {
+             let userRow = rows.filter(row => row["Discord Username"] == message.author.tag);
+              //console.log(userRow[0])
 
-            const sheet = doc.sheetsByTitle["Form responses 1"];
-            const rows = await sheet.getRows();
-            const cells = await sheet.loadCells('B2:D5')
-           
-          try {
-              /* let userRow = rows.filter(row => row["Discord Username"] == message.author.tag);
-                
-                userRow[0]["Test 26.07.2022"] = "1"
-                await userRow[0].save();
-                message.react("✅")
-                message.author.send("I added you to our list. We will send out Oculus invitations about 30 minutes before the test. Don't forget to check our <#783287669613527041> if you run into any issues or ask for help in <#724969092506976286>! looking forward to see you in the Stadium")
-              */message.author.send("Hm. Sorry I can't sign you up right now. We have to wait until another test is announced! 👀")
-            }
-            catch (e) {
-              //  message.react("✉️")
-              //  message.author.send("Looks like I have trouble finding your Confidentiality Agreement. 🤔 Did you maybe change your Discord tag (name#1234)? You can update it in your form submission and try again afterwards! ")
-            }
-        }
-        
-    }
+              userRow[0]["Test"] = "1"
+              await userRow[0].save();
+
+              message.react("✅")
+              message.author.send("I added you to our list. We will send out Oculus invitations about 30 minutes before the test. Don't forget to check our <#783287669613527041> if you run into any issues or ask for help in <#724969092506976286>! looking forward to see you in the Stadium")
+            //message.author.send("Hm. Sorry I can't sign you up right now. We have to wait until another test is announced! 👀")
+          }
+          catch (e) {
+            message.author.send("Looks like I have trouble finding your Confidentiality Agreement. 🤔 Did you maybe change your Discord tag (name#1234)? You can update it in your form submission and try again afterwards!")
+          }
+      }
+      }
+  }
 };
-
