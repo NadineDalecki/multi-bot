56e4513fa3df9ee3bc8efe6a4c2beaa718ecdb8b
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-01-19 21:45:17 +0200

    dialogflow update

--- dialogflow/KVN.js
+++ dialogflow/KVN.js
@@ -7,8 +7,19 @@ module.exports = {
             message.cleanContent.startsWith(client.user.username + " ") ||
             message.cleanContent.startsWith(client.user.username.toLowerCase() + " ")) {
 
+            if ( message.mentions.has(client.user.id) ||
+            message.cleanContent.startsWith(client.user.username + " ") ||
+            message.cleanContent.startsWith(client.user.username.toLowerCase() + " ")) {
+
+            cleanMessage = message.split(' ').slice(1).join(' ')
+            const answer = await functions.DialogflowQuery(client, cleanMessage);
+            console.log(cleanMessage)
+            message.reply(answer.response);
+            }
+            else {
             const answer = await functions.DialogflowQuery(client, message);
             message.reply(answer.response);
+            }
         }
     }
 };
--- functions.js
+++ functions.js
@@ -113,19 +113,19 @@ module.exports = {
                 newEmbed.setFooter(embed[0].Footer_Text, embed[0].Footer_Avatar_URL);
             }
             if (embed[0].Field_1_Title && embed[0].Field_1_Text) {
-                newEmbed.addField(embed[0].Field_1_Title, embed[0].Field_1_Text, embed[0].Field_1_Inline);
+                newEmbed.addFields([{name: embed[0].Field_1_Title, value: embed[0].Field_1_Text, inline: embed[0].Field_1_Inline}]);
             }
             if (embed[0].Field_2_Title && embed[0].Field_2_Text) {
-                newEmbed.addField(embed[0].Field_2_Title, embed[0].Field_2_Text, embed[0].Field_2_Inline);
+                newEmbed.addFields([{name: embed[0].Field_2_Title, value: embed[0].Field_2_Text, inline: embed[0].Field_2_Inline}]);
             }
             if (embed[0].Field_3_Title && embed[0].Field_3_Text) {
-                newEmbed.addField(embed[0].Field_3_Title, embed[0].Field_3_Text, embed[0].Field_3_Inline);
+                newEmbed.addFields([{name: embed[0].Field_3_Title, value: embed[0].Field_3_Text, inline: embed[0].Field_3_Inline}]);
             }
             if (embed[0].Field_4_Title && embed[0].Field_4_Text) {
-                newEmbed.addField(embed[0].Field_4_Title, embed[0].Field_4_Text, embed[0].Field_4_Inline);
+                newEmbed.addFields([{name: embed[0].Field_4_Title, value: embed[0].Field_4_Text, inline: embed[0].Field_4_Inline}]);
             }
             if (embed[0].Field_5_Title && embed[0].Field_5_Text) {
-                newEmbed.addField(embed[0].Field_5_Title, embed[0].Field_5_Text, embed[0].Field_5_Inline);
+                newEmbed.addFields([{name: embed[0].Field_5_Title, value: embed[0].Field_5_Text, inline: embed[0].Field_5_Inline}]);
             }
             return newEmbed;
         } catch (e) {
