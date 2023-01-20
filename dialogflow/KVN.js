63743f82dcfb34326a9039f7a40f618f3e5e837d
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-01-20 11:22:04 +0200

    kvn in

--- dialogflow/KVN.js
+++ dialogflow/KVN.js
@@ -7,8 +7,21 @@ module.exports = {
             message.cleanContent.startsWith(client.user.username + " ") ||
             message.cleanContent.startsWith(client.user.username.toLowerCase() + " ")) {
 
-            const answer = await functions.DialogflowQuery(client, message);
-            message.reply(answer.response);
+            if (message.mentions.has(client.user.id) ||
+                message.cleanContent.startsWith(client.user.username + " ") ||
+                message.cleanContent.startsWith(client.user.username.toLowerCase() + " ")) {
+
+                cleanMessage = message.substr(original.indexOf(" ") + 1)
+                console.log(cleanMessage)
+
+                const answer = await functions.DialogflowQuery(client, cleanMessage);
+                message.reply(answer.response);
+
+            }
+            else {
+                const answer = await functions.DialogflowQuery(client, message);
+                message.reply(answer.response);
             }
         }
     }
+}
