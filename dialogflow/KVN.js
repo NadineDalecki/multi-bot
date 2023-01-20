5f8e0994482ed58eccb423479da6155ef9a9c407
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-01-20 11:35:20 +0200

    ...

--- dialogflow/KVN.js
+++ dialogflow/KVN.js
@@ -11,15 +11,15 @@ module.exports = {
                 message.cleanContent.startsWith(client.user.username + " ") ||
                 message.cleanContent.startsWith(client.user.username.toLowerCase() + " ")) {
 
-                cleanMessage = message.cleanContent.substr(message.cleanContent.indexOf(" ") + 1)
-                console.log(cleanMessage)
+                messageWithoutName = message.cleanContent.substr(message.cleanContent.indexOf(" ") + 1)
+                console.log(messageWithoutName)
 
-                const answer = await functions.DialogflowQuery(client, cleanMessage);
+                const answer = await functions.DialogflowQuery(client, messageWithoutName, message);
                 message.reply(answer.response);
 
             }
             else {
-                const answer = await functions.DialogflowQuery(client, message);
+                const answer = await functions.DialogflowQuery(client, message.cleanContent, message);
                 message.reply(answer.response);
             }
         }
