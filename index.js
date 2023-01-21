634c20aff3f0485ee1c3e09ce2fb86c4a2e1693c
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-01-21 22:30:44 +0200

    ...

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const set = require("./settings.json")
 const functions = require("./functions.js")
 const giphy = require("giphy-api")(process.env.GIPHY)
 const schedule = require("node-schedule")
+const { MessageEmbed } = require("discord.js");
 
 const BotTokens = [process.env.BOT_CASTER, process.env.BOT_MEL, process.env.BOT_AFFEN, process.env.BOT_ITSY, process.env.BOT_KVN, process.env.BOT_TG, process.env.BOT_IGLE, process.env.BOT_HERMES, process.env.BOT_EWAN]
 
