fd71246e8520fe0fd1b2e4adfe2e16b0698000cb
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-05-06 22:41:16 +0300

    ...

--- dialogflow/BB8.js
+++ dialogflow/BB8.js
@@ -4,10 +4,12 @@ module.exports = {
 		messageWithoutName = ""
 		if (message.mentions.has(client.user.id) || message.cleanContent.toLowerCase().startsWith(client.user.username.toLowerCase() + " ")) {
 			messageWithoutName = message.cleanContent.substr(message.cleanContent.indexOf(" ") + 1)
+			messageWithCharacter = `${set[client.user.username].character} ${messageWithoutName}`
+		functions.OpenAIAnswer(client, message, messageWithCharacter)
 		} else if (message.channel.type == "DM" || message.cleanContent.toLowerCase().includes(client.user.username.toLowerCase())) {
 			messageWithoutName = message.cleanContent
-		}
-		messageWithCharacter = `${set[client.user.username].character} ${messageWithoutName}`
+			messageWithCharacter = `${set[client.user.username].character} ${messageWithoutName}`
 		functions.OpenAIAnswer(client, message, messageWithCharacter)
+		}
 	}
 }
