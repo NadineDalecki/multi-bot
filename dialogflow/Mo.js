5b7643d0283ce1cf8c2577d889d49bcd08228f38
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-05-06 16:18:41 +0300

    ...

--- dialogflow/Mo.js
+++ dialogflow/Mo.js
@@ -1,40 +1,21 @@
 module.exports = {
-    name: "Mo",
-    execute: async function (client, message, functions, set) {
+	name: "Mo",
+	execute: async function (client, message, functions, set) {
+		messageWithoutName = undefined
 
-        if (message.channel.type == "DM" ||
-            message.mentions.has(client.user.id) ||
-            message.cleanContent.startsWith(client.user.username + " ") ||
-            message.cleanContent.startsWith(client.user.username.toLowerCase() + " ")) {
+		if (message.mentions.has(client.user.id) || message.cleanContent.startsWith(client.user.username + " ") || message.cleanContent.startsWith(client.user.username.toLowerCase() + " ")) {
+			messageWithoutName = message.cleanContent.substr(message.cleanContent.indexOf(" ") + 1)
+		} else if (message.channel.type == "DM") {
+			messageWithoutName = message.cleanContent
+		}
 
-            if (message.mentions.has(client.user.id) ||
-                message.cleanContent.startsWith(client.user.username + " ") ||
-                message.cleanContent.startsWith(client.user.username.toLowerCase() + " ")) {
+		console.log(messageWithoutName)
 
-                messageWithoutName = message.cleanContent.substr(message.cleanContent.indexOf(" ") + 1)
-               
-                const answer = await functions.DialogflowQuery(client, messageWithoutName, message);
-                if (answer.intent === "Default Fallback Intent") {
-                functions.OpenAIAnswer(client, message)
-                }
-                else {
-                message.reply(answer.response);
-                }
-            }
-            else {
-                const answer = await functions.DialogflowQuery(client, message.cleanContent, message);
-                if (answer.intent === "Default Fallback Intent") {
-                    functions.OpenAIAnswer(client, message)
-                    }
-                    else {
-                    message.reply(answer.response);
-                    }
-
-                console.log(answer)
-            }
-        }
-    }
+		const answer = await functions.DialogflowQuery(client, messageWithoutName, message)
+		if (answer.intent === "Default Fallback Intent") {
+			functions.OpenAIAnswer(client, message)
+		} else {
+			message.reply(answer.response)
+		}
+	}
 }
-
-
-//
