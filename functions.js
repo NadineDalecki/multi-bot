2576cba86c03c96a611eedc7e6fa657dc0721e5a
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-05-07 10:55:06 +0300

    ...

--- functions.js
+++ functions.js
@@ -13,7 +13,7 @@ module.exports = {
 	CleanMessage: function(client, message) {
 		console.log("Before: " + message.cleanContent)
 		cleanMessage = message.cleanContent
-		if (message.cleanContent.toLowerCase().startsWith(client.user.username.toLowerCase() + " ") || message.cleanContent.startsWith("@Affen")) {
+		if (message.cleanContent.toLowerCase().startsWith(client.user.username.toLowerCase() + " ") || message.cleanContent.toString().startsWith("@Affen")) {
 			cleanMessage = message.cleanContent.substr(message.cleanContent.indexOf(" ") + 1)
 		}
 			else {
