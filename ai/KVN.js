f6d3f3a0dd175bfbd0906ed0b5c9fb5151e0bfad
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-05-07 20:07:32 +0300

    ...

--- ai/KVN.js
+++ ai/KVN.js
@@ -2,8 +2,7 @@ module.exports = {
 	name: "KVN",
 	execute: async function (client, message, functions, set) {
 		if (message.channel.type == "DM") {
-			const text = functions.CleanMessage(client, message)
-			functions.OpenAIAnswer(client, message, text)
+			functions.OpenAIAnswer(client, message)
 		}
 	}
 }
