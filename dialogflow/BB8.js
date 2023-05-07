badd5a9aee4413b693aed8b446eed6815840bcbe
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-05-07 12:11:30 +0300

    ...

--- dialogflow/BB8.js
+++ dialogflow/BB8.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "BB8",
 	execute: async function (client, message, functions, set) {
-		if (message.mentions.has(client.user.id) || message.cleanContent.toLowerCase().startsWith(client.user.username.toLowerCase() + " ") || message.channel.type == "DM") {
+		if (message.mentions.has(client.user.id) || message.cleanContent.toLowerCase().includes(client.user.username.toLowerCase()) || message.channel.type == "DM") {
 			const text = functions.CleanMessage(client, message)
 			functions.OpenAIAnswer(client, message, text)
 		}
