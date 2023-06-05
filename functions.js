061a653f623e2e2ebcf8a1daa6e40f538a3ac379
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-06-05 14:14:08 +0300

    ...

--- functions.js
+++ functions.js
@@ -158,7 +158,7 @@ module.exports = {
 		try {
 			const completion = await openai.createCompletion(
 				{
-					model: "text-davinci-003",
+					model: "text-curie",
 					prompt: `${set[client.user.username].character} ${cleanMessage}.`,
 					max_tokens: 1000
 				},
