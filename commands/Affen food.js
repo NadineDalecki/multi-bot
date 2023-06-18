cd6d97c42048ea804fa4ae101098bb25b6a40f3e
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-06-18 16:18:48 +0300

    ...

--- commands/Affen food.js
+++ commands/Affen food.js
@@ -22,17 +22,17 @@ module.exports = {
 						url: "https://www.reddit.com/r/foodporn.json",
 						method: "get"
 					})
+					var randomNum = Math.floor(Math.random() * foodItem.data.data.dist)
+					const image = foodItem.data.data.children[randomNum].data.url
+					const title = foodItem.data.data.children[randomNum].data.title
+
+					const embed = new MessageEmbed().setColor("#edabea").setAuthor({ name: message.author.username, iconURL: message.author.avatarURL(), url: "https://cdn0.iconfinder.com/data/icons/most-usable-logos/120/Reddit-512.png" }).setTitle(title).setImage(image).setFooter({ text: "From r/FoodPorn", iconURL: "https://cdn0.iconfinder.com/data/icons/most-usable-logos/120/Reddit-512.png" })
+
+					message.channel.send({ embeds: [embed] })
+					cooldowns.set(cooldownKey, now)
 				} catch (e) {
 					message.channel.send("r/FoodPorn is protesting against reddits API billing...nothing I can do about it. Maybe it will work again later!")
 				}
-				var randomNum = Math.floor(Math.random() * foodItem.data.data.dist)
-				const image = foodItem.data.data.children[randomNum].data.url
-				const title = foodItem.data.data.children[randomNum].data.title
-
-				const embed = new MessageEmbed().setColor("#edabea").setAuthor({ name: message.author.username, iconURL: message.author.avatarURL(), url: "https://cdn0.iconfinder.com/data/icons/most-usable-logos/120/Reddit-512.png" }).setTitle(title).setImage(image).setFooter({ text: "From r/FoodPorn", iconURL: "https://cdn0.iconfinder.com/data/icons/most-usable-logos/120/Reddit-512.png" })
-
-				message.channel.send({ embeds: [embed] })
-				cooldowns.set(cooldownKey, now)
 			} else {
 				message.channel.send("Wrong channel you noob. Use #incredible-edible.")
 			}
