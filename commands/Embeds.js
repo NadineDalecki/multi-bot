2fe9339f46115001f0ff8b60825382ac9c2b682e
na_da <20108524+NadineDalecki@users.noreply.github.com>
2023-08-08 10:49:47 +0300

    embed check

--- commands/Embeds.js
+++ commands/Embeds.js
@@ -7,16 +7,14 @@ module.exports = {
             message.member.roles.cache.some(r => adminRoles.includes(r.id)) ||
             message.member.hasPermission("ADMINISTRATOR")
         ) {
+            console.log(message)
             const data = await functions.SpreadsheetGET(client);
             const sheet = data.doc.sheetsByTitle["Embeds"];
             const rows = await sheet.getRows();
 
             let embed = rows.filter(embed => embed.name == args.join(" "));
+            console.log(embed)
             const finalEmbed = functions.EmbedBuilder(embed);
-            console.log(embed[0].mention)
-            if(embed[0].mention){
-                message.channel.send(embed[0].mention)
-            }
             message.channel.send({ embeds: [finalEmbed] });;
         }
     }
