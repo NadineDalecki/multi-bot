f0d031409416362c3e3dc8840222ec48a5427404
na_da <20108524+NadineDalecki@users.noreply.github.com>
2024-01-07 16:22:54 +0200

    ...

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function runBot(token) {
 	client.on("messageCreate", async message => {
 		if (client.user.id != message.author.id && !message.author.bot && !(message.content.includes("@here") || message.content.includes("@everyone"))) {
 			if (client.user.username == "Affen" && message.content.startsWith("spark://c/")){
-				message.reply([Link](`${message.cleanContent}`))
+				message.reply([Link](`https://${message.cleanContent}`))
 			}
 			if (message.content.startsWith(set[client.user.username].prefix)) {
 				functions.Command(client, message, functions, set, MessageEmbed)
