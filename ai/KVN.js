656bcbfdd15be92372a02e106280834b714e3c8f
na_da <20108524+NadineDalecki@users.noreply.github.com>
2025-01-21 13:03:20 +0200

    Remove KVN

--- ai/KVN.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: "KVN",
-	execute: async function (client, message, functions, set) {
-		if (message.channel.type == "DM") {
-			client.channels.cache.get("718176504437276682").send(mesage.author.tag + " | " + message.cleanContent + " | " + message.link)
-			functions.OpenAIAnswer(client, message)
-		}
-	}
-}
