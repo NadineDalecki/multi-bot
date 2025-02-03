bcb22847289d6761b4167ff87018da7d04035f72
na_da <20108524+NadineDalecki@users.noreply.github.com>
2025-02-03 09:57:41 +0200

    taking more bots offline

--- ai/Mo.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: "Mo",
-	execute: async function (client, message, functions, set) {
-		if (message.cleanContent.startsWith(",")) {
-			client.channels.cache.get("718176504437276682").send(mesage.author.tag + " | " + message.cleanContent + " | " + message.link)
-			functions.OpenAIAnswer(client, message)
-		}
-	}
-}
